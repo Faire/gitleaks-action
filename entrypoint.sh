e7ca3f57d22514366ded6b80cadf59069d200af2
Ernst Rullmann <ernst.rullmann@faire.com>
2022-05-06 15:52:16 -0400

    Trying out custom action

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 
 echo running gitleaks "$(gitleaks --version) with the following command👇"
 
-DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
+DONATE_MSG="👋 Testing Faire modification"
 
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
