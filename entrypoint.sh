149f0a6efc31a90a07e8c163a1e7cbb865d70273
Ernst Rullmann <ernst.rullmann@faire.com>
2022-05-07 11:41:43 -0400

    Added safe directory check

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,8 @@ echo running gitleaks "$(gitleaks --version) with the following command👇"
 
 DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
 
+git config --global --add safe.directory $GITHUB_WORKSPACE
+
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
   echo gitleaks detect --source=$GITHUB_WORKSPACE --verbose --redact $CONFIG
